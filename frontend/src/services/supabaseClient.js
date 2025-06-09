f9edd2796b8478934502ed582f2da080391acbf8
Emirhan Sergen ÇERGEL <117258974+EmirhanSergen@users.noreply.github.com>
2025-06-09 23:12:36 +0300

    Use env vars for Supabase client

--- frontend/src/services/supabaseClient.js
+++ frontend/src/services/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase configuration
-const supabaseUrl = 'https://jfqmvaeeelneeawbvkga.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmcW12YWVlZWxuZWVhd2J2a2dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3MDAwNzgsImV4cCI6MjA2MTI3NjA3OH0.B9R9mUucwZCYXSuZPD3PKGcNUfR58Zb4Efb0kyUe2RM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
